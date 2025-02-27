216e7e199e8a43226496fe7ffa65ae424b272b0b
Margaux Vandererven <margaux.vandereven@gmail.com>
2025-02-27 11:19:06 +0100

    ajout

---
+++ utilities.py
@@ -0,0 +1,114 @@
+from imports import *
+
+mol=[]
+lines_BD221742 = {}
+
+
+
+# files = {
+#     "9000-15000_10042024.bsyn": element,
+#     "turbo_atoms.20180901_TS2020_transitions_mod_xx_ABO.txt": element.upper(),
+#     "17000-25000_10042024.bsyn": element,
+#     # "12C16O_14500-25000.bsyn": "CO", 
+#     # "13C17O_14500-25000.bsyn": "CO", 
+
+# }
+
+# base_path = "/Users/margauxvandererven/Library/CloudStorage/OneDrive-UniversitéLibredeBruxelles/memoire/Linelists/Sophie_IGRINS/"
+
+def get_ew_atom(data, ew_limit, Teff, particular_element=None):
+    base_path = "/Users/margauxvandererven/Library/CloudStorage/OneDrive-UniversitéLibredeBruxelles/memoire/Linelists/Sophie_IGRINS/"
+    files = {
+    "9000-15000_10042024.bsyn": particular_element,
+    "turbo_atoms.20180901_TS2020_transitions_mod_xx_ABO.txt": particular_element.upper() if particular_element and isinstance(particular_element, str) else particular_element,
+    "17000-25000_10042024.bsyn": particular_element}
+
+    for file, atom in files.items():
+        with open(base_path + file, "r", encoding="utf8") as f:
+            for line in f:
+                parts = line.split()
+                if len(parts) > 5:
+                    # Cas 1: particular_element spécifié
+                    if particular_element and particular_element in line:
+                        # print(parts)
+                        wavelength, excitation_potential, loggf = map(float, parts[:3])
+                        ew = 10**(loggf - (5040 / Teff) * excitation_potential)
+                        element_name = parts[11][1].upper() + parts[11][2:].lower() + " " + parts[12]
+                        if ew_limit < ew < 1e-9 and wavelength > 14500:
+                            data.append((wavelength, excitation_potential, ew, loggf, element_name))
+                    
+                    # Cas 2: pas de particular_element spécifié
+                    elif not particular_element:
+                        wavelength, excitation_potential, loggf = map(float, parts[:3])
+                        ew = 10**(loggf - (5040 / Teff) * excitation_potential)
+                        element_name = parts[11][1].upper() + parts[11][2:].lower() + " " + parts[12]
+                        if ew > ew_limit and wavelength > 14500:
+                            data.append((wavelength, excitation_potential, ew, loggf, element_name))
+
+    return {
+        "wavelength": [d[0] for d in data],
+        "excitation_potential": [d[1] for d in data],
+        "ew": [d[2] for d in data],
+        "loggf": [d[3] for d in data],
+        "element": [d[4] for d in data]
+    }
+
+print(get_ew_atom(data=[], ew_limit=1e-10, Teff=4000, particular_element="Fe I")["wavelength"])
+
+
+
+
+
+# get_ew(mol, files, base_path, 1e-10, Teff)
+# pprint(mol)
+# for d in data:
+#      lines_BD221742[d[3]] = []
+# for d in data:
+#     lines_BD221742.get(d[3]).append(d[0])
+
+
+# from collections import defaultdict
+
+# # Initialisation du dictionnaire
+# lines_BD221742 = defaultdict(list)
+
+# # Trier les données par d[3] (élément) puis par d[0] (wavelength)
+# data_sorted = sorted(data, key=lambda x: (x[3], x[0]))
+
+# # Dictionnaire temporaire pour stocker la meilleure valeur de d[2] par élément et par plage de d[0]
+# best_values = {}
+
+# for wavelength, excitation_potential, ew, element in data_sorted:
+#     # Vérifier s'il existe déjà une valeur proche dans la liste
+#     if element in best_values:
+#         close_wavelengths = [w for w in best_values[element] if abs(w - wavelength) < 0.2]
+
+#         if close_wavelengths:
+#             # Vérifier si la nouvelle valeur de d[2] (ew) est plus grande que celle existante
+#             closest_wavelength = close_wavelengths[0]
+#             if ew > best_values[element][closest_wavelength]:  
+#                 best_values[element][closest_wavelength] = ew  # Mise à jour avec le plus grand d[2]
+#         else:
+#             best_values[element][wavelength] = ew  # Ajouter un nouveau point si aucun proche n'existe
+#     else:
+#         best_values[element] = {wavelength: ew}  # Initialiser pour cet élément
+
+# # Convertir best_values en dictionnaire {d[3]: [d[0]]}
+# for element, wavelengths in best_values.items():
+#     lines_BD221742[element] = list(wavelengths.keys())
+
+# # Affichage du dictionnaire final
+# # pprint(lines_BD221742)
+
+
+def latex_table(dictionnaire, element):
+    sorted_items = sorted(dictionnaire.items())
+    latex_table = "\\begin{table}[h!]\n\\centering\n\\small\n\\begin{tabular}{ccc}\n\\hline\n\\hline\n"\
+                f"$\\lambda_{{\\mathrm{{{element}}}}}$ & $\\lambda_{{\\mathrm{{min}}}}$ & $\\lambda_{{\\mathrm{{max}}}}$ \\\\\n\\hline\n"
+
+    for key, value in sorted_items:
+        latex_table += f"{key:.2f} & {value[0]:.2f}& & {value[1]:.2f}&{value[2]:.2f}& {value[3]:.2f}&{value[4]:.3f}\\\\\n"
+
+    latex_table += "\\end{tabular}\n\\end{table}"
+
+    print(latex_table)
